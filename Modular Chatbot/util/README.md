d9425446fb3754591aeea7ab7e562199f937bd52
Muhammad Ahsan Shahid <ahsan.shahiid@gmail.com>
2021-10-14 18:35:55 +0200

    Delete Modular Chatbot/util directory

--- Modular Chatbot/util/README.md
+++
@@ -1,6 +0,0 @@
-# Modular Chatbot
-
-This repository contains source codes for the modular chatbot. The folders are:
-
-* **framework/** Stores the framework itself
-* **intent_categorization_example_rasa/** an intent classifier example code using Spacy and RASA NLU
--- Modular Chatbot/util/framework/unittests.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from frankenbot.test.nlu_test import TestKeywordIntentDetector
-from frankenbot.test.modul_test import TestAtom
-from frankenbot.test.response_generator_test import TestSimpleResponseGenerator
-from frankenbot.test.strategy_test import TestMaxActivationStrategy
-from frankenbot.test.bot_test import TestBot
-
-if __name__ == '__main__':
-    unittest.main()
