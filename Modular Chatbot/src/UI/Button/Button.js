c44478b588e543cb7d06efa86646b9e48db55b7d
Muhammad Ahsan Shahid <ahsan.shahiid@gmail.com>
2021-10-14 18:34:51 +0200

    Delete Modular Chatbot/src directory

--- Modular Chatbot/src/UI/Button/Button.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './Button.css';
-
-const Button = props => <button {...props} className="custombutton"><span>{props.text}</span></button>
-
-
-export default Button;
